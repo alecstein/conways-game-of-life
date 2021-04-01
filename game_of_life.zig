52f6c7d2ecdc45f8a7b44ac5d6e34d1ad0e7c807
Alec Stein <alecstein@g.ucla.edu>
2021-04-01 00:30:35 -0500

    rebuilt with Grid class

--- game_of_life.zig
+++ game_of_life.zig
@@ -1,131 +1,100 @@
-// const c = @cImport({
-//     @cInclude("ncurses.h");
-// });
-
-//const cstd = @cImport(@cInclude("stdlib.h"));
 const std = @import("std");
-const stdout = std.io.getStdOut().writer();
-const print = stdout.print;
-const writeAll = stdout.writeAll;
 
 pub fn main() !void {
 
-    // _ = c.initscr();
-    // _ = c.refesh();
-
-    // var xmax: usize = undefined;
-    // var ymax: usize = undefined;
-
-
-    // initialize the grid 
-
-    var grid = initGrid();
+    var grid = Grid(40, 140).init();
+    grid.printToScr();
 
     while (true) {
-        std.time.sleep(50000000);
-        try print("\x1B[2J\x1B[H", .{});
-        try printGrid(grid);
-        grid = updateGrid(grid);
+        std.time.sleep(30000000);
+        grid.update();
+        grid.printToScr();
     }
 }
 
-//const r = std.rand.Random;
-var rng = std.rand.DefaultPrng.init(0);
+fn Grid(comptime height: usize, comptime width: usize) type {
+    return struct {
+        const Self = @This();
+        height: usize,
+        width: usize,
+        grid: [height][width]u8,
 
-fn initGrid() [40][100][]const u8 {
-    var grid: [40][100][]const u8 = undefined;
-    for (grid) |row, i| {
-        for (row) |cell, j| {
-            const edge = (j == 99 or j == 0 or i == 39 or i == 0);
-            // const r = @rem(cstd.rand(), 10);
-            const r = rng.random.int(u8) % 20;
-            if (edge) { 
-                grid[i][j] = " "; 
+        pub fn init() Self {
+            var rang = std.rand.DefaultPrng.init(0);
+            var grid: [height][width]u8 = undefined;
+            for (grid) |row, i| {
+                for (row) |cell, j| {
+                    const edge = (j == width - 1 or j == 0 or i == height - 1 or height == 0); // fix this
+                    const r = rang.random.int(u8) % 20; // fix this
+                    if (edge) {
+                        grid[i][j] = 0;
+                    } else if (r < 5) {
+                        grid[i][j] = 1;
+                    } else {
+                        grid[i][j] = 0;
+                    }
+                }
             }
-            else if (r < 5) {
-                grid[i][j] = "▮";
-            }
-            else {
-                grid[i][j] = " ";
-            }
+            return Self{
+                .height = height,
+                .width = width,
+                .grid = grid,
+            };
         }
-    }
-    return grid;
-}
 
-fn printGrid(grid: [40][100][]const u8) !void {
-    for (grid) |row| {
-        try writeAll("\n");
-        for (row) |cell| {
-            try print("{s}", .{cell});
-        }
-    }
-}
-
-fn updateGrid(grid: [40][100][]const u8) [40][100][]const u8 {
-
-    var new_grid = initGrid();
-
-    for (grid) |row, i| {
-        for (row) |cell, j| {
-
-            const edge = (j == 99 or j == 0 or i == 39 or i == 0);
-            if (edge) {
-                continue;
-            }
-
-            var alive: bool = undefined;
-
-            if (std.mem.eql(u8, cell, "▮")) {
-                alive = true;
-            }
-
-            else {
-                alive = false;
-            }
-
-            const delt = [3]u8{0,1,2};
-            var alive_neighbors: u8 = 0;
-            for (delt) |x, s| {
-                for (delt) |y, t| {
-                    if (x == 1 and y == 1) {
+        pub fn update(self: *Self) void {
+            var new_grid: [height][width]u8 = undefined;
+            for (new_grid) |row, i| {
+                for (row) |cell, j| {
+                    const edge = (j == width - 1 or j == 0 or i == height - 1 or i == 0); // fix this
+                    if (edge) {
+                        new_grid[i][j] = 0;
                         continue;
                     }
-                    else {
-                        if (std.mem.eql(u8, grid[i+x-1][j+y-1], "▮")) {
-                            alive_neighbors += 1;
+                    var alive_neighbors: u8 = 0;
+                    const delt = [3]usize{ 0, 1, 2 };
+                    for (delt) |x| {
+                        for (delt) |y| {
+                            alive_neighbors += self.grid[i + x - 1][j + y - 1];
+                        }
+                    }
+                    alive_neighbors -= self.grid[i][j];
+
+                    if (self.grid[i][j] == 1) {
+                        if (alive_neighbors < 2) {
+                            new_grid[i][j] = 0;
+                        }else if (alive_neighbors == 2 or alive_neighbors == 3) {
+                            new_grid[i][j] = 1;
+                        } else {
+                            new_grid[i][j] = 0;
+                        }
+                    } else {
+                        if (alive_neighbors == 3) {
+                            new_grid[i][j] = 1;
+                        } else {
+                            new_grid[i][j] = 0;
                         }
                     }
                 }
             }
 
-            // any live cell with fewer than two live neighbors dies
-            // any live cell with two or three live neighbours lives on to the next generation
-            // any live cell with more than three live neighbours dies, as if by overpopulation.
-            if (alive) {
-                if (alive_neighbors < 2) {
-                    new_grid[i][j] = " ";
+            self.grid = new_grid;
+            
+        }
+        pub fn printToScr(self: Self) void {
+            std.debug.print("\x1B[2J\x1B[H", .{});
+            for (self.grid) |row| {
+                // std.debug.print("\n", .{});
+                for (row) |cell| {
+                    var v: []const u8 = undefined;
+                    if (cell == 0) {
+                        v = " ";
+                    } else {
+                        v = "▮";
+                    }
+                    std.debug.print("{s}", .{v});
                 }
-                if (alive_neighbors == 2 or alive_neighbors == 3) {
-                    new_grid[i][j] = "▮";
-                }
-                else {
-                    new_grid[i][j] = " ";
-                }
-            }
-
-            // any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-            else {
-                if (alive_neighbors == 3) {
-                    new_grid[i][j] = "▮";
-                }
-                else {
-                    new_grid[i][j] = " ";
-                }
-                
             }
         }
-    }
-    return new_grid;
+    };
 }
