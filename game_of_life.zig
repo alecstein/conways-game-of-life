f070d94f672ecd299b9807ec58eed3cff3189343
Alec Stein <alecstein@g.ucla.edu>
2021-03-31 19:28:27 -0500

    working -- no ncurses -- just terminal print

--- game_of_life.zig
+++ game_of_life.zig
@@ -1,38 +1,131 @@
+// const c = @cImport({
+//     @cInclude("ncurses.h");
+// });
+
+//const cstd = @cImport(@cInclude("stdlib.h"));
 const std = @import("std");
-const c = @cImport({
-    @cInclude("ncurses.h");
-});
+const stdout = std.io.getStdOut().writer();
+const print = stdout.print;
+const writeAll = stdout.writeAll;
 
 pub fn main() !void {
 
-    _ = c.initscr();
-    _ = c.raw();
-    _ = c.printw("Hello");
-    _ = c.move(12, 13); 
-    _ = c.mvprintw(15,20, "Movement");
-    _ = c.mvprintw(17, 25, "Another");
-    _ = c.mvaddch(12,50, 99);
+    // _ = c.initscr();
+    // _ = c.refesh();
 
-    _ = c.getch();
-    _ = c.endwin();
+    // var xmax: usize = undefined;
+    // var ymax: usize = undefined;
 
-    return;
-}
 
-fn Grid(comptime T: type) type {
-    return struct {
-        const Self = @This();
+    // initialize the grid 
 
-        var width: u8 = undefined;
-        var height: u8 = undefined;
+    var grid = initGrid();
 
+    while (true) {
+        std.time.sleep(50000000);
+        try print("\x1B[2J\x1B[H", .{});
+        try printGrid(grid);
+        grid = updateGrid(grid);
     }
 }
 
-fn iterate(grid_in: Grid) Grid {
-    // takes in a grid and returns a grid
+//const r = std.rand.Random;
+var rng = std.rand.DefaultPrng.init(0);
 
-    grid_out = Grid;
+fn initGrid() [40][100][]const u8 {
+    var grid: [40][100][]const u8 = undefined;
+    for (grid) |row, i| {
+        for (row) |cell, j| {
+            const edge = (j == 99 or j == 0 or i == 39 or i == 0);
+            // const r = @rem(cstd.rand(), 10);
+            const r = rng.random.int(u8) % 20;
+            if (edge) { 
+                grid[i][j] = " "; 
+            }
+            else if (r < 5) {
+                grid[i][j] = "▮";
+            }
+            else {
+                grid[i][j] = " ";
+            }
+        }
+    }
+    return grid;
+}
 
+fn printGrid(grid: [40][100][]const u8) !void {
+    for (grid) |row| {
+        try writeAll("\n");
+        for (row) |cell| {
+            try print("{s}", .{cell});
+        }
+    }
+}
 
+fn updateGrid(grid: [40][100][]const u8) [40][100][]const u8 {
+
+    var new_grid = initGrid();
+
+    for (grid) |row, i| {
+        for (row) |cell, j| {
+
+            const edge = (j == 99 or j == 0 or i == 39 or i == 0);
+            if (edge) {
+                continue;
+            }
+
+            var alive: bool = undefined;
+
+            if (std.mem.eql(u8, cell, "▮")) {
+                alive = true;
+            }
+
+            else {
+                alive = false;
+            }
+
+            const delt = [3]u8{0,1,2};
+            var alive_neighbors: u8 = 0;
+            for (delt) |x, s| {
+                for (delt) |y, t| {
+                    if (x == 1 and y == 1) {
+                        continue;
+                    }
+                    else {
+                        if (std.mem.eql(u8, grid[i+x-1][j+y-1], "▮")) {
+                            alive_neighbors += 1;
+                        }
+                    }
+                }
+            }
+
+            // any live cell with fewer than two live neighbors dies
+            // any live cell with two or three live neighbours lives on to the next generation
+            // any live cell with more than three live neighbours dies, as if by overpopulation.
+            if (alive) {
+                if (alive_neighbors < 2) {
+                    new_grid[i][j] = " ";
+                }
+                if (alive_neighbors == 2 or alive_neighbors == 3) {
+                    new_grid[i][j] = "▮";
+                }
+                else {
+                    new_grid[i][j] = " ";
+                }
+            }
+
+            // any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+            else {
+                if (alive_neighbors == 3) {
+                    new_grid[i][j] = "▮";
+                }
+                else {
+                    new_grid[i][j] = " ";
+                }
+                
+            }
+        }
+    }
+    return new_grid;
 }
